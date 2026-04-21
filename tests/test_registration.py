b75f553814e250843b34c7fa95c479cbc43d1ddc
whyouseless <zxcasdqwe300882@gmail.com>
2026-04-21 23:58:16 +0300

    Практикуемся в реализации PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -10,4 +10,4 @@ def test_successful_registration(registration_page: RegistrationPage, dashboard_
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
     registration_page.fill_registration_form('user.name@gmail.com', 'username', 'password')
     registration_page.click_registration_button()
-    dashboard_page.check_dashboard_title()
+    dashboard_page.check_visible_dashboard_title()
